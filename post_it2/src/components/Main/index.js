7e3dd943e6c32e52e4c178a87582e4366400288e
Davi Guilherme <job.davic@gmail.com>
2021-11-29 22:16:40 -0300

    OIasd

--- post_it2/src/components/Main/index.js
+++ post_it2/src/components/Main/index.js
@@ -5,44 +5,48 @@ import {VscAdd,VscChevronRight} from 'react-icons/vsc';
 import Post_It from "../post_it_card";
 import './style.css';
 import {useState} from 'react';
+import { TiArrowLeftOutline,TiArrowRightOutline} from "react-icons/ti";
 
 
 export default function Main(props){
-    let Postit = [{titulo:'a',corpo:'corpo'},]
+    let Posts = [{id:0,titulo:'a',corpo:'corpo'}]
     const [Page,SetPage] = useState(0);
 
     function HandeClick(){
         document.getElementById('sidebar').classList.toggle('sidebar-opened')
         document.getElementById('circle').classList.toggle('sidebar')
     }
-     
+    function getPosts(){
+        
+    }
 
     return(
             <div className="all_Main">
                 <div className="add_post_it"><VscAdd/></div>
                 <div className="sidebar-closed" id="sidebar" >
-                    <div className="sidebar_icon"><BiHome e width="100" height="100"/>OI</div>
-                    <div className="sidebar_icon"><BiCode/></div>
-                    <div className="sidebar_icon"><BiMeteor/></div>
+                    <div className="sidebar_icon"><BiHome e width="2em" height="100"/>Home</div>
+                    <div className="sidebar_icon"><BiCode/>Code</div>
+                    <div className="sidebar_icon"><BiMeteor/>Contatos</div>
                     <div className="circle-animation" id="circle" onClick={HandeClick}><BiRightArrow/></div>
                 </div>
 
-            <div className="content-grid">
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                <Post_It titulo="Head" conteudo ="Conteudo"/>
-                
+            <div className="content" id="content">
+                <div className="arrow"><TiArrowLeftOutline width="100px"/></div>
+                <div className="content-grid" id="content_grid">
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    <Post_It titulo="oi" conteudo="oi"/>
+                    <Post_It titulo="oi" conteudo="oi"/>
+
+                </div>
+                <div className="arrow"> <TiArrowRightOutline/></div>
             </div>
-            
             
         </div>
     );
--- post_it2/src/components/post_it_card/index.js
+++ post_it2/src/components/post_it_card/index.js
@@ -4,9 +4,11 @@ export default function Post_It(props){
 
     return(
         <div className="post_it_card">
-            {props.titulo}
+
+            <h1>{props.titulo}</h1>
 
             <p>{props.conteudo}</p>
+            
 
         </div>
     );
